5a18f1396f849c725eb5b9145c467f7b75bc6bdf
Aric Rachmany <Aric.Rachmany@dh.com>
2023-01-29 18:05:29 +0200

    add subscriber to search search value changes

--- src/app/ui/components/header-components/search/search.component.ts
+++ src/app/ui/components/header-components/search/search.component.ts
@@ -1,29 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {Router} from '@angular/router';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, OnDestroy {
+
+  private subscriber: Subscription;
 
   search = new FormControl();
 
-  constructor(private router: Router, ) { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
     this.watchSearchValueChanges();
   }
 
   watchSearchValueChanges(): void {
-    this.search.valueChanges.subscribe((data: any) => {
+    this.subscriber = this.search.valueChanges.subscribe((data: any) => {
       data ?
         this.router.navigate(['/products'], { queryParams: { search: data } }) :
         this.router.navigate(['/products']);
     });
   }
 
-
+  ngOnDestroy(): void {
+    this.subscriber.unsubscribe();
+  }
 }
