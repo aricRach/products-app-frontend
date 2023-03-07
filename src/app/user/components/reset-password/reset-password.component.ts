46ce9abcef2d4bd5449a49274e5878cb22d9b070
Aric Rachmany <Aric.Rachmany@dh.com>
2023-03-07 18:57:13 +0200

    re active reset password

--- src/app/user/components/reset-password/reset-password.component.ts
+++ src/app/user/components/reset-password/reset-password.component.ts
@@ -1,16 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {UserService} from '../../services/user.service';
-import {AngularFireAuth} from '@angular/fire/auth';
 import {Router} from '@angular/router';
-import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.component.html',
   styleUrls: ['../authentication-styles.css']
 })
-export class ResetPasswordComponent implements OnInit {
+export class ResetPasswordComponent {
 
   errorMsg: string;
   resetPasswordForm = new FormGroup({
@@ -23,19 +21,13 @@ export class ResetPasswordComponent implements OnInit {
 
   constructor(private userService: UserService, private router: Router) { }
 
-  ngOnInit(): void {}
-
   onSubmit(): void {
-      // this.userService.forgetPassword(this.resetPasswordForm.value.email).subscribe(() => {
-      //   this.router.navigate(['/login'], {state: {
-      //     message: 'check your inbox for password reset link'
-      //   }});
-      // }, error => {
-      //   console.error(error);
-      // });
-    this.router.navigate(['/login'], {state: {
-        message: 'check your inbox for password reset link'
-    },
-    });
+      this.userService.forgetPassword(this.resetPasswordForm.value.email).subscribe(() => {
+        this.router.navigate(['/login'], {state: {
+          message: 'check your inbox for password reset link'
+        }});
+      }, error => {
+        console.error(error);
+      });
   }
 }
